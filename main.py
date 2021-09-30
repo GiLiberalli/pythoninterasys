b461f30237389684c5acf03860d4ab7ffe215b69
USER <liberalligiovanna@gmail.com>
2021-09-29 22:42:37 -0300

    Commit: Testes Unitários
    Consulta de Dados pelo o arquivo "csv"

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
 # 1 - imports - bibliotecas
 import pytest
-
 # 2 - class - classe
-
 
 # 3 - definitions - definições = métodos e funções
 def print_hi(name):
     print(f'Oi, {name}')
 
+
 def somar(numero1, numero2):
-    return numero1 + numero2 #bug
+    return numero1 + numero2
 
 def subtrair(numero1, numero2):
-    return numero1 - numero2
+    return numero1 / numero2 # bug!!
 
 def multiplicar(numero1, numero2):
     return numero1 * numero2
 
+# esse é um exemplo de demonstração
 def dividir(numero1, numero2):
     if numero2 != 0:
         return numero1 / numero2
     else:
         return 'Não dividirás por zero'
 
-#Testes Unitários/ Teste de Unidades
+def dividir_try_except(numero1, numero2):
+    try:
+        return numero1 / numero2
+    except TypeError:
+        #return 'Não dividirás por zero'
+        if TypeError == ZeroDivisionError:
+            return 'Não dividirás por zero'
+        elif TypeError == ArithmeticError:
+            return 'Erro no cálculo'
+        elif TypeError == ValueError:
+            return 'Erro no valor'
+        else:
+            return 'Erro desconhecido'
+        pass
 
-   #teste da função de somar
+# Testes Unitarios / Teste de Unidades
+
+ # teste da função de somar
+def test_somar_didatico():
+    # 1 - Configura / Prepara
+    numero1 = 8 # input / entrada
+    numero2 = 5 # input / entrada
+    resultado_esperado = 13 # output / saida
+    # 2 - Executa
+    resultado_atual = somar(numero1,numero2)
+    # 3 - Check / Valida
+    assert resultado_atual == resultado_esperado
 
 @pytest.mark.parametrize('numero1,numero2,resultado',[
     #valores
@@ -34,30 +58,55 @@ def dividir(numero1, numero2):
     (10,6, 16), # teste 3
 ])
 def test_somar(numero1, numero2, resultado):
-    assert somar(numero1,numero2) == resultado
+    try:
+        assert somar(numero1,numero2) == resultado
+    except AssertionError:
+        print(f'Entrou no Except: {AssertionError}')
+        pass
 
-
-      #1 - Configura/Prepara
-       #numero1 = 8  #input / entrada
-       #numero2 = 5  #input / entrada
-       #resultado_esperado = 13  #output / saída
-
-       #2 - Executa
-
-       #resultado_atual = somar(numero1, numero2)
-
-       #3 Check / Valida
-       #assert resultado_atual == resultado_esperado
-
-def test_somar_compacto():
-    assert somar(8,5) == 13
+def test_somar_resultado_negativo():
+    assert somar(-1000,-2000) == -3000
 
 def test_subtrair():
-    assert  subtrair(4,5) == -1
+    assert subtrair(4,5) == -1
+
+def test_multiplicar():
+    assert multiplicar(3,7) == 21
+
+def test_dividir():
+    assert dividir(8,4) == 2
+
+def test_dividir_por_zero():
+    assert dividir(8,0) == 'Não dividirás por zero'
+
+
+
+@pytest.mark.parametrize('numero1, numero2, resultado',[
+    (8,2,4),
+    (20,4,5),
+    (10,0,'Não dividirás por zero')
+])
+def test_dividir_try_except(numero1,numero2,resultado):
+    assert dividir_try_except(numero1,numero2) == resultado
+
+    # teste positivo --> mostrar o resultado correto
+    #                --> avançar para a próxima etapa
+
+    # teste negativo --> mostrar a mensagem de erro
+
+# Dia 1 : 100 testes : 0 passaram
+# Dia 2 : 100 testes : 5 passaram
+# Dia 3 : 100 testes : 15 passaram
+# Dia 4 : 100 testes : 30 passaram
+
+#TDD : Desenvolvimento Direcionado pelo Testes
+# - Criar o esqueleto de classes, funções e métodos logo no início da Sprint
+# - Criar pelo 1 teste (feliz) para todas as funções e métodos
+# - Executar todos os testes unitários diariamente para medir o progresso
+
 
 if __name__ == '__main__':
-
-    print_hi('Giovanna')
+    print_hi('Jose')
 
     # soma de 2 números
     resultado = somar(4,2)
@@ -74,6 +123,3 @@ if __name__ == '__main__':
     # divisão
     resultado = dividir(9,8)
     print(f'O resultado da divisão: {resultado}')
-
-
-
---
+++ usuario.py
@@ -0,0 +1,74 @@
+# 1 - imports
+import json
+
+import pytest
+import csv
+import requests
+
+teste_dados_novos_usuarios = [
+
+    (1, 'Juca', 'Pirama', 'juca@interasys.com.br') ,             # usuario 1
+    (2, 'Agatha', 'Cristine', 'agatha@interasys.com.br')         # usuario 2
+]
+
+teste_dados_usuarios_atuais = [
+
+    (1, 'George', 'Bluth', 'george.bluth@reqres.in') ,
+    (2, 'Janet', 'Weaver', 'janet.weaver@reqres.in')         # usuario 2
+]
+
+# CRUD/ ICAE                   #Traduções
+# Aplicações   APIS            APIs
+# Create       Post            Incluir/Publicar
+# Reach / Research  Get        Consultar/Pegar
+# Update  Put                  Atualizar
+# Delete  Delete               Excluir
+
+@pytest.mark.parametrize('id,nome,sobrenome,email', teste_dados_usuarios_atuais)
+
+def testar_dados_usuarios(id, nome, sobrenome, email, json_response=None):  # função que testa o algo
+    from requests import HTTPError
+    try:
+        response = requests.get(f'https://reqres.in/api/users/{id}')
+        jsonResponse = response.json()
+        id_obtido = jsonResponse['data']['id']
+        nome_obtido = jsonResponse['data']['first_name']
+        sobrenome_obtido = jsonResponse['data']['last_name']
+        email_obtido = jsonResponse['data']['email']
+
+        print(f'id: {id_obtido} - nome: {nome_obtido} - sobrenome: {sobrenome_obtido} - email: {email_obtido}')
+        print(json.dumps(json_response, indent=2, sort_keys=True))
+
+        assert id_obtido == id
+        assert nome_obtido == nome
+        assert sobrenome_obtido == sobrenome
+        assert email_obtido == email
+
+    except HTTPError as http_fail :  # Para o ISTQB, descobriu rodando que é falha
+        print(f'Um erro de HTTP aconteceu: {http_fail}')
+    except Exception as fail:
+        print(f'Falha Inesperada:{fail}')
+
+
+ #funcion que testa
+ #função que faz algo --> Fora do meu computador
+ #API que vamos usar para fazer o teste:
+ # https://reqres.in/api/users
+
+#Leitor do arquivo csv:
+
+def ler_dados_do_csv():
+    teste_dados_csv = []
+    nome_arquivo = 'usuarios.csv'
+    try:
+       with open(nome_arquivo,newline='') as csvfile:
+           dados = csv.reader(csvfile,delimiter = ',')
+           next(dados)
+           for linha in dados:
+               teste_dados_csv.append(linha)
+       return teste_dados_csv
+    except FileNotFoundError:
+        print('Arquivo Não Encontrado')
+    except Exception as fail:
+        print(f'Falha Imprevista: {fail}')
+
